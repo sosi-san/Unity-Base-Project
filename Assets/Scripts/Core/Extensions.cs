12ebfc5d7349f0c80084bfce5fe11eaddba2f344
Adam Woska <ademwoska@gmail.com>
2022-06-26 13:51:20 +0200

    Extensions script and Scripts folder added

---
+++ Assets/Scripts/Core/Extensions.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Woska
+{
+    public class Extensions : MonoBehaviour
+    {
+        #region Public Fields
+        #endregion
+
+        #region Unity Method
+
+        private void Awake()
+        {
+            
+        }
+        private void Start()
+        {
+            
+        }
+        private void Update()
+        {
+            
+        }
+
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
